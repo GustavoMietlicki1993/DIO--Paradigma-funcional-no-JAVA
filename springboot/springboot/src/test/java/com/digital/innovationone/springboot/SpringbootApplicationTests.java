d98b300aed4a5f04af27b7517c73f3dfa5faa218
Gustavo Rodrigues Mietlicki <gustavo.mietlicki@deal.br>
2020-04-16 23:44:35 -0300

    Primeiro Commit

---
+++ springboot/springboot/src/test/java/com/digital/innovationone/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.innovationone.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
